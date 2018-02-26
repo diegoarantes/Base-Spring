2d5b396589d9bdc901fa8b99a761fb648ca2d1cd
D.Arantes <D.Arantes@TI-002.INCOPOSTES.COM>
2018-02-26 17:36:25 -0300

    Adicionado Serializable

--- src/main/java/br/com/absoft/model/Usuario.java
+++ src/main/java/br/com/absoft/model/Usuario.java
@@ -1,5 +1,6 @@
 package br.com.absoft.model;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,20 +9,23 @@ import javax.persistence.Id;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Entity	
+@Entity
 @Data
 @EqualsAndHashCode(of = "id")
-public class Usuario {	
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	private String email;
-	
-	private String senha;	
-	
-	private String nome;
-	
-	private boolean ativo;	
+@SuppressWarnings("PersistenceUnitPresent")
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String senha;
+
+    private String nome;
+
+    private boolean ativo;
 }
